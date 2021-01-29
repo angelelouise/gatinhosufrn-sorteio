58168cea0eb2dc7bce154ec1d3831685a1899f4c
Natã Hermes <nhermesdev@gmail.com>
2021-01-28 23:26:30 -0300

    Adicionado container para injeção de dependências do módulo de participantes

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import IParticipantsRepository from '@modules/participants/repositories/IParticipantsRepository';
+import ParticipantsRepository from '@modules/participants/infra/typeorm/repositories/ParticipantsRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IParticipantsRepository>(
+  'ParticipantsRepository',
+  ParticipantsRepository,
 );
