d01d4ee60bc0a22b304e70ab05316e89d0f123d5
Natã Hermes <nhermesdev@gmail.com>
2021-01-28 22:34:52 -0300

    Ajuste nas rotas de usuários

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,11 +1,8 @@
 import { Router } from 'express';
 
 import multer from 'multer';
-import { getRepository } from 'typeorm';
 
 import uploadConfig from '@config/upload';
-
-import User from '@modules/users/infra/typeorm/entities/User';
 
 import UsersController from '../controllers/UsersController';
 import UserAvatarController from '../controllers/UserAvatarController';
@@ -19,18 +16,7 @@ const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const users = getRepository(User);
-
-  const user = await users.findOne({
-    where: { id },
-  });
-
-  return res.json(user);
-});
-
-usersRouter.post('/', upload.single('file_idea'), usersController.create);
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
   '/avatar',
