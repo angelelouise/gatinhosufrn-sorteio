a526b28335d0026216d014aa675531a7b039f2c8
Natã Hermes <nhermesdev@gmail.com>
2021-01-28 23:16:23 -0300

    Adicionado rota de acessa ao módulo de participante

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import participantsRouter from '@modules/participants/infra/http/routes/participants.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/participants', participantsRouter);
 
 export default routes;
